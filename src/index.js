4e111d2b51af68dd1be7b0490fa8f35b3dbc2715
levy002 <levy.ukwi002@gmail.com>
2022-03-21 20:49:40 +0200

    Add entry .js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react"
+import React, { StrictMode } from "react"
 import ReactDOM from "react-dom"
+//component file
+import TodoContainer from "./components/TodoContainer";
+import "./App.css"
 
-const element = <h1>Hello from Create React App</h1>
-
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(
+    <StrictMode>
+      <TodoContainer />
+    </StrictMode>,
+    document.getElementById("root")
+)
