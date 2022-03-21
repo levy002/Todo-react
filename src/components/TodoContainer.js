41d5c48ceaeb2744a08b6373962b56394c2d2140
levy002 <levy.ukwi002@gmail.com>
2022-03-21 23:34:42 +0200

    Add lifecycle methods and mantain local storage

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,23 +6,7 @@ import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
 
   handleChange = (id) => {
@@ -59,6 +43,23 @@ class TodoContainer extends Component {
       todos: [...this.state.todos, newTodo],
     });
   };
+
+ componentDidMount() {
+  const temp = localStorage.getItem("todos")
+  const loadedTodos = JSON.parse(temp)
+  if (loadedTodos) {
+    this.setState({
+      todos: loadedTodos
+    })
+  }
+}
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem("todos", temp);
+    }
+  }
 
   setUpdate = (updatedTitle, id) => {
     this.setState({
