bdeaac444824397bc4a4dbd16ce09510a6c84cb0
levy002 <levy.ukwi002@gmail.com>
2022-03-22 12:16:03 +0200

    Add new pages

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <>
+      <div>hello 'Levy' from about page</div>
+    </>
+  );
+};
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotMatch = () => {
+  return (
+    <>
+      <div>
+        <h3>Hello Esperado Munilo.......</h3>
+      </div>
+    </>
+  );
+};
+export default NotMatch;
