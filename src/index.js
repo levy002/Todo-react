e0e2d34d40674ad58e3f39208f40fd2e7174fcd7
levy002 <levy.ukwi002@gmail.com>
2022-03-22 12:17:29 +0200

    Add routes to the pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TodoContainer from './components/TodoContainer';
 import './App.css';
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 ReactDOM.render(
   <StrictMode>
-    <TodoContainer />
+    <Router>
+      <nav>
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link to="/">Home</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<TodoContainer />} />
+        <Route path="/About" element={<About />} />
+        <Route path="Contact" element={<Contact />} />
+      </Routes>
+    </Router>
   </StrictMode>,
   document.getElementById('root'),
 );
