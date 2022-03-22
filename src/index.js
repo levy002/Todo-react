8f11dacd834fc943181a97cd3f1c69e7e4307962
levy002 <levy.ukwi002@gmail.com>
2022-03-22 13:30:11 +0200

    deploy with github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TodoContainer from './components/TodoContainer';
 import './App.css';
 import About from "./pages/About";
@@ -9,14 +9,14 @@ import Navbar from './components/Navbar'
 
 ReactDOM.render(
   <StrictMode>
-    <HashRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<TodoContainer />} />
         <Route path="/About" element={<About />} />
         <Route path="Contact" element={<Contact />} />
       </Routes>
-    </HashRouter>
+    </Router>
   </StrictMode>,
   document.getElementById('root'),
 );
